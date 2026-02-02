b35f1a04284600e5fa0c6885b6e421472e02c9a6
Sam Aripov <169399701+samaripov@users.noreply.github.com>
2026-02-02 17:55:51 -0500

    CRUD scaffold for movie

---
+++ BlazorWebAppMovies/Data/BlazorWebAppMoviesContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BlazorWebAppMovies.Model;
+
+namespace BlazorWebAppMovies.Data
+{
+    public class BlazorWebAppMoviesContext : DbContext
+    {
+        public BlazorWebAppMoviesContext (DbContextOptions<BlazorWebAppMoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlazorWebAppMovies.Model.Movie> Movie { get; set; } = default!;
+    }
+}
--- BlazorWebAppMovies/obj/Debug/net10.0/BlazorWebAppMovies.AssemblyInfo.cs
+++ BlazorWebAppMovies/obj/Debug/net10.0/BlazorWebAppMovies.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorWebAppMovies")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4be95d0bbb85984d85013889f362b3bbbc2fcd5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorWebAppMovies")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorWebAppMovies")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
